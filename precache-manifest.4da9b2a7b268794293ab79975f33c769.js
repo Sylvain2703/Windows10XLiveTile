6132a277664f442c701a84e87ae39a071b2bc740
Sylvain Bruyère <sylvain_bruyere@live.fr>
2020-03-09 01:23:44 +0100

    v0.1.0

---
+++ precache-manifest.4da9b2a7b268794293ab79975f33c769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15154c40ce563207d8f5494e0dac77dc",
+    "url": "/Windows10XLiveTile/index.html"
+  },
+  {
+    "revision": "12fbf2653f4a58da1594",
+    "url": "/Windows10XLiveTile/static/css/main.34c34749.chunk.css"
+  },
+  {
+    "revision": "6741289d3ef2d5709d31",
+    "url": "/Windows10XLiveTile/static/js/2.b56598d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Windows10XLiveTile/static/js/2.b56598d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12fbf2653f4a58da1594",
+    "url": "/Windows10XLiveTile/static/js/main.31a0fc43.chunk.js"
+  },
+  {
+    "revision": "62c8ea7796edf84795dc",
+    "url": "/Windows10XLiveTile/static/js/runtime-main.93ae54ff.js"
+  }
+]);
